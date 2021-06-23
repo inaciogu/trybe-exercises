7db4a51657bd18894244d4e4a396c8c1c3bd5e38
Gustavo <gus.inacio@hotmail.com>
2021-06-23 19:00:09 -0300

    Exericicio usando tipo primitivo boolean

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_5.js
@@ -0,0 +1,11 @@
+let angle1 = 30;
+let angle2 = 49;
+let angle3 = 101;
+let isvalid = true;
+let isntvalid = false;
+
+if (angle1 + angle2 + angle3 == 180) {
+    console.log(isvalid)
+} else {
+    console.log(isntvalid)
+}

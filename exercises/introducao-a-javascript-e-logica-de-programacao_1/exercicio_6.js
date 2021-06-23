9e95b9026d3ef927aae818d6835055e732fc3ae7
Gustavo <gus.inacio@hotmail.com>
2021-06-23 19:29:51 -0300

    Exercicio usando condicionais e tipo boolean

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_6.js
@@ -0,0 +1,21 @@
+let bishop = true;
+let chessHorse = true;
+let mermaid = false
+
+if (bishop==true) {
+    console.log('bishop: diagonals')
+} else {
+    console.log('peça invalida')
+}
+if (chessHorse==true) {
+    console.log('chess horse: L-Movements')
+
+}else {
+    console.log('peça invalida')
+}
+if (mermaid==true) {
+    console.log('mermaid: mermaid-movements')
+} else {
+    console.log('peça invalida')
+}
+
